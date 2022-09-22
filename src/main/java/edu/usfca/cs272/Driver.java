27cbd14054940f574563180a6b012d73041cae5d
mzkahane <mzkahane@dons.usfca.edu>
2022-09-22 16:58:07 -0700

    Added fileExtension function, fixed directory indexing

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -27,6 +27,23 @@ import java.util.stream.Stream;
 public class Driver {
 
 	/**
+	 * Gets the file extension for the file at a given path
+	 *
+	 * @param textPath the path that points to the file
+	 * @return a string containing the file extension
+	 */
+	public static String fileExtension(Path textPath) {
+		String extension = "";
+		String path = textPath.toString();
+
+		int i = path.lastIndexOf('.');
+		if (i > 0) {
+			extension = path.substring(i+1);
+		}
+		return extension;
+	}
+
+	/**
 	 * Inputs the contents of a file to the word index
 	 * @param path the path where the file is found
 	 * @param index the index to input the contents into
@@ -48,20 +65,27 @@ public class Driver {
 	 * @param textPath the path to find the files
 	 * @param indexPath the path to output the index
 	 * @param index the index to parse the files into
+	 * @param isDirectory indicates whether the textPath points to a directory or not
 	 * @throws IOException when an IO error occurs
 	 */
-	public static void findAndInput(Path textPath, Path indexPath, WordIndex index) throws IOException {
+	public static void findAndInput(Path textPath, Path indexPath, WordIndex index, boolean isDirectory) throws IOException {
 		if (Files.isDirectory(textPath)) {
 			try (Stream<Path> files = Files.walk(textPath)) {
 				List<Path> paths = files.filter(Files::isRegularFile).collect(Collectors.toList());
 				Collections.sort(paths);
 				for (int i = 0; i < paths.size(); i++) {
-					findAndInput(paths.get(i), indexPath, index);
+					findAndInput(paths.get(i), indexPath, index, isDirectory);
 				}
 			}
 		} else if (Files.isReadable(textPath)) {
-			inputFile(textPath, index);
-			//PrettyJsonWriter.writeIndex(index, indexPath, 0);
+			String extension = fileExtension(textPath);
+			extension = extension.toLowerCase();
+			if (isDirectory && (extension.equals("txt") || extension.equals("text"))) {
+				inputFile(textPath, index);
+			}
+			if (!isDirectory) {
+				inputFile(textPath, index);
+			}
 		} else {
 			System.out.println("invalid path");
 			return;
@@ -92,6 +116,7 @@ public class Driver {
 		Path indexPath = Path.of("index.json");
 		Path textPath = null;
 		boolean indexFound = false;
+		boolean isDirectory  = false;
 
 		for (String flag : flags.viewFlags()) {
 			if (Objects.equals(flag, "-index")) {
@@ -110,15 +135,16 @@ public class Driver {
 			BufferedWriter writer = Files.newBufferedWriter(indexPath, UTF_8);
 			writer.write("[]");
 			return;
+		} else if (Files.isDirectory(textPath)) {
+			isDirectory = true;
 		}
 
 		if (!indexFound) {
 			return;
 		}
 
-		findAndInput(textPath, indexPath, index);
+		findAndInput(textPath, indexPath, index, isDirectory);
 
-		//System.out.println(indexPath.toAbsolutePath().toString());
 		PrettyJsonWriter.writeIndex(index, indexPath, 0);
 
 		// calculate time elapsed and output
