b33aaf80d376461d6f7327fa3dd71948fb9cc7a1
mzkahane <mzkahane@dons.usfca.edu>
2022-11-01 16:27:06 -0700

    Issue with multi-word queries
    multi-word queries are being split per word and output as separate
    entries under the same query

--- src/main/java/edu/usfca/cs272/WordSearcher.java
+++ src/main/java/edu/usfca/cs272/WordSearcher.java
@@ -23,22 +23,32 @@ public class WordSearcher {
 	 * @return an ArrayList of the paths that have at least one occurrence of
 	 * one of the query words.
 	 */
-	private static TreeMap<String, ArrayList<Path>> findResults(TreeSet<String> query, WordIndex index, boolean exact) {
-		// TODO Changed this to a map so that the results can be traced back to the word they were
-		// found for. this should make it easier to do the partial and the exact search...
-		// avoid at least one of the for loops in search()
-		TreeMap<String, ArrayList<Path>> results = new TreeMap<>();
+	private static TreeMap<String, LinkedHashMap<Path, Integer>> findResults(TreeSet<String> query, WordIndex index, boolean exact) {
+		/*
+		 * FIXME the two word queries are being split into two entries for that
+		 * query in the output. Need to somehow add them both together, combining
+		 * their appearance counts.
+		 *
+		 * might be able to do this here in the collection
+		 * phase, by getting rid of the outer layer and, if the path is already in
+		 * the map, add to the word count the position size of the second word.
+		 *
+		 * If it has to be done in the processing phase in search(), I might need to
+		 * do something similar, see if the location is already in the scoredResults,
+		 * if so, update the count and score.
+		 * */
+		TreeMap<String, LinkedHashMap<Path, Integer>> results = new TreeMap<>();
 		if (exact) {
 			for (String word : query) {
 				if (index.has(word)) {
 					var temp = index.get(word);
 
 					for (Path location : temp.keySet()) {
-						if (results.containsKey(word) && !results.get(word).contains(location)) {
-							results.get(word).add(location);
+						if (results.containsKey(word) && !results.get(word).containsKey(location)) {
+							results.get(word).put(location, index.size(word, location));
 						} else if (!results.containsKey(word)) {
-							results.put(word, new ArrayList<Path>());
-							results.get(word).add(location);
+							results.put(word, new LinkedHashMap<Path, Integer>());
+							results.get(word).put(location, index.size(word, location));
 						}
 					}
 				}
@@ -51,11 +61,14 @@ public class WordSearcher {
 						var temp = index.get(key);
 
 						for (Path location : temp.keySet()) {
-							if (results.containsKey(key) && !results.get(key).contains(location)) {
-									results.get(key).add(location);
-							} else if(!results.containsKey(key)) {
-								results.put(key, new ArrayList<Path>());
-								results.get(key).add(location);
+							if (results.containsKey(word) && !results.get(word).containsKey(location)) {
+									results.get(word).put(location, index.size(key, location));
+							} else if(!results.containsKey(word)) {
+								results.put(word, new LinkedHashMap<Path, Integer>());
+								results.get(word).put(location, index.size(key, location));
+							} else if(results.containsKey(word) && results.get(word).containsKey(location)) {
+								int  newCount = results.get(word).get(location) + index.size(key, location);
+								results.get(word).put(location, newCount);
 							}
 						}
 					}
@@ -81,24 +94,21 @@ public class WordSearcher {
 	 */
 	public static ArrayList<LinkedHashMap<String, String>> search
 	(TreeSet<String> query, WordIndex index, boolean exact) {
-		TreeMap<String, ArrayList<Path>> results = findResults(query, index, exact);
+		TreeMap<String, LinkedHashMap<Path, Integer>> results = findResults(query, index, exact);
 		ArrayList<LinkedHashMap<String, String>> scoredResults = new ArrayList<>();
 
+		String queryWord;
+		Path location;
 		for (String word : results.keySet()) {
-			int appearances = 0;
-			Path location = null; //TODO add a null check?
-			for (Path path : results.get(word)) {
+			queryWord = word;
+			for (Path path : results.get(queryWord).keySet()) {
 				location = path;
-				appearances += index.get(word).get(location).size();
 
-				String appearanceCount = String.format("%d", appearances);
-				String score = String.format("%.8f", (double) appearances/index.getWordCount(location.toString()));
+				LinkedHashMap<String, String> temp = new LinkedHashMap<>();
+				int appearances = results.get(queryWord).get(location);
+				temp.put("count", String.format("%d", appearances));
+				temp.put("score", String.format("%.8f", (double) appearances/index.getWordCount(location.toString())));
 				String where = ('"' + location.toString() + '"');
-
-				LinkedHashMap<String, String> temp = new LinkedHashMap<String, String>();
-
-				temp.put("count", appearanceCount);
-				temp.put("score", score);
 				temp.put("where", where);
 
 				int j = scoredResults.size();
@@ -126,6 +136,7 @@ public class WordSearcher {
 			}
 		}
 
+/* ---- Exact search works, but partial doesn't ---- */
 //		for (Path location : results) {
 //			int appearances = 0;
 //
